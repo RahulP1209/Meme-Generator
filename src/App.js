6df1856fb6559a3caf7d70fdd34b8db2e36c79bc
Rahul Pandey <rahul120901@gmail.com>
2024-03-04 18:48:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg'
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../images/Troll Face.png"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img  className= "header--image" src={logo} alt="logo" />
+            <h2 className= "header--title">Meme Generator</h2>
+            <h4 className= "header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
